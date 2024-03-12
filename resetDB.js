37625994c1b6c60b881c293896ace48c3a8162b9
galKatzav <galoosh1591@gmail.com>
2024-03-12 15:28:31 +0200

    reserDB&userRoute

--- resetDB.js
+++ resetDB.js
@@ -1,5 +1,7 @@
 const db = require("./db");
 const Product = require("./models/Product");
+const User = require("./models/User");
+const Task = require("./models/Task");
 
 const sampleProducts = [
   { name: "apple", price: 42 },
@@ -7,18 +9,70 @@ const sampleProducts = [
   { name: "melon", price: 666 },
 ];
 
+const sampleUsers = [
+  {
+    _id: "60d8d87b89a2652038a4e28f",
+    name: "gal",
+    email: "gal@gmail.com",
+    password: "gal",
+  },
+  {
+    _id: "60d8d87b89a2652038a4e290",
+    name: "viky",
+    email: "viky@gmail.com",
+    password: "viky",
+  },
+  {
+    _id: "60d8d87b89a2652038a4e291",
+    name: "toy",
+    email: "toy@gmail.com",
+    password: "toy",
+  },
+];
+
+const sampleTasks = [
+  {
+    title: "Complete project proposal",
+    userID: "60d8d87b89a2652038a4e28f" /* ObjectId of a user - gal */,
+  },
+  {
+    title: "Prepare presentation slides",
+    userID: "60d8d87b89a2652038a4e290" /* ObjectId of a user - viky */,
+  },
+  {
+    title: "Review code changes",
+    userID: "60d8d87b89a2652038a4e291" /* ObjectId of a user - toy*/,
+  },
+];
+
 main();
 
 async function main() {
   db.connect();
 
-  console.log('deleting all products...');
-  await Product.deleteMany();
-  console.log('complete!');
+  // console.log("deleting all products...");
+  // await Product.deleteMany();
+  // console.log("complete!");
 
-  console.log('inserting products...');
-  await Product.insertMany(sampleProducts);
-  console.log('complete!');
+  // console.log("inserting products...");
+  // await Product.insertMany(sampleProducts);
+  // console.log("complete!");
+
+  console.log("deleting all users...");
+  await User.deleteMany();
+  console.log("complete!");
+
+  console.log("inserting users...");
+  await User.insertMany(sampleUsers);
+  console.log("complete!");
+
+  console.log("deleting all tasks...");
+  await Task.deleteMany();
+  console.log("complete!");
+
+  console.log("inserting tasks...");
+  await Task.insertMany(sampleTasks);
+  console.log("complete!");
 
   db.disconnect();
 }
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,17 +4,22 @@ const jwt = require("jsonwebtoken");
 const router = express.Router();
 const User = require("../models/User");
 
+// .../users
+
+// Gets all the users from the DB
 router.get("/", async (req, res) => {
   const allUsers = await User.find();
   res.json(allUsers);
 });
 
+// Gets specific user from the DB by id
 router.get("/:id", async (req, res) => {
   const givenID = req.params.id;
   const wantedUser = await User.findById(givenID);
   res.json(wantedUser);
 });
 
+// Create new user
 router.post("/", async (req, res) => {
   try {
     const givenPass = req.body.password;
@@ -34,6 +39,7 @@ router.post("/", async (req, res) => {
   }
 });
 
+// Need to put the real pass and email
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
@@ -52,16 +58,61 @@ router.post("/login", async (req, res) => {
   }
 });
 
+// Need only the token when the user login
 router.post("/whoami", async (req, res) => {
   const token = req.body.token;
   const user = jwt.verify(token, "123");
   res.send(user.name);
 });
 
+// Need to id that i want to delete
 router.delete("/:id", async (req, res) => {
   const givenID = req.params.id;
   await User.findByIdAndDelete(givenID);
   res.send(`user with id ${givenID} was deleted`);
 });
 
+router.patch("/update-user", async (req, res) => {
+  try {
+    const { token, name } = req.body;
+
+    if (!token) {
+      return res.status(400).send("Token is required");
+    }
+
+    const user = jwt.verify(token, "123");
+
+    if (!user) {
+      return res.status(401).send("Invalid token");
+    }
+
+    // Apply updates to the user object
+    if (name) {
+      user.name = name;
+    }
+    console.log(user.name);
+
+    // Send back the updated user object in the response
+    res.json(user);
+  } catch (error) {
+    console.error("Error in PATCH /update-user:", error);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
+
+// token name galk1 pass galk1
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZ2FsIGsxIiwiaWF0IjoxNzEwMjQ3MzQ5fQ.j65qMMDsShcygjhYzZedApzuldtwEZkEM_bBkPfcn_U
+
+/*
+// {
+//     "email": "galTheQueen@gmail.com",
+//     "password": "galTheQueen"
+// }
+
+// {
+//     "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZ2FsVGhlUXVlZW4iLCJpYXQiOjE3MTAyNDgxNjR9.lhUx_uiXVtC2ZyzFEd6Zb2-D3eMwE3LpFxpAcudZToM",
+//     "name": "galUpd"
+// }
+*/
